dd8ca592cf5ef0ff0523afef5bb06b7bb9ea458f
ANTONIO-PC\Catalinoiu <acatalinoiu@gmail.com>
2018-01-06 20:10:56 +0200

    added SigleListItem

--- app/src/main/java/recognizer/com/ocr/FileAndDirectoryActivity.java
+++ app/src/main/java/recognizer/com/ocr/FileAndDirectoryActivity.java
@@ -2,12 +2,17 @@ package recognizer.com.ocr;
 
 
         import android.app.ListActivity;
+        import android.content.Intent;
         import android.os.Environment;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
         import android.view.Menu;
         import android.view.MenuItem;
+        import android.view.View;
+        import android.widget.AdapterView;
         import android.widget.ArrayAdapter;
+        import android.widget.ListView;
+        import android.widget.TextView;
 
         import java.io.File;
         import java.util.ArrayList;
@@ -23,6 +28,24 @@ public class FileAndDirectoryActivity extends ListActivity {
 
         File root = new File(path);
         ListDir(root);
+        ListView lv = getListView();
+
+        // listening to single list item on click
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // selected item
+                String num = ((TextView) view).getText().toString();
+
+                // Launching new Activity on selecting single List Item
+                Intent i = new Intent(getApplicationContext(), SingleListItem.class);
+                // sending data to new activity
+                i.putExtra("number", num);
+                startActivity(i);
+
+            }
+        });
     }
 
     void ListDir(File f) {
---
+++ app/src/main/java/recognizer/com/ocr/SingleListItem.java
@@ -0,0 +1,26 @@
+package recognizer.com.ocr;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.samples.vision.ocrreader.R;
+
+
+public class SingleListItem extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_main);
+
+        TextView txtProduct = (TextView) findViewById(R.id.text_value);
+
+        Intent i = getIntent();
+        // getting attached intent data
+        String product = i.getStringExtra("number");
+        // displaying selected product name
+        txtProduct.setText(product);
+
+    }
+}

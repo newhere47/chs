4d621e177654e8671f0ddfd98af082460dfa57ec
ANTONIO-PC\Catalinoiu <acatalinoiu@gmail.com>
2018-01-07 12:17:52 +0200

    added ReadFile methode

--- app/src/main/java/recognizer/com/ocr/FileAndDirectoryActivity.java
+++ app/src/main/java/recognizer/com/ocr/FileAndDirectoryActivity.java
@@ -2,10 +2,12 @@ package recognizer.com.ocr;
 
 
         import android.app.ListActivity;
+        import android.content.Context;
         import android.content.Intent;
         import android.os.Environment;
         import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
+        import android.util.Log;
         import android.view.Menu;
         import android.view.MenuItem;
         import android.view.View;
@@ -14,14 +16,22 @@ package recognizer.com.ocr;
         import android.widget.ListView;
         import android.widget.TextView;
 
+        import java.io.BufferedReader;
         import java.io.File;
+        import java.io.FileInputStream;
+        import java.io.FileNotFoundException;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
         import java.util.ArrayList;
         import java.util.List;
+
+        import static android.content.ContentValues.TAG;
 
 public class FileAndDirectoryActivity extends ListActivity {
 
     private List<String> fileList = new ArrayList<String>();
     public final String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/readerFiles";
+    String line = null;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,17 +48,20 @@ public class FileAndDirectoryActivity extends ListActivity {
                 // selected item
                 String num = ((TextView) view).getText().toString();
 
+
+
                 // Launching new Activity on selecting single List Item
                 Intent i = new Intent(getApplicationContext(), SingleListItem.class);
                 // sending data to new activity
-                i.putExtra("number", num);
+                i.putExtra("number",  ReadFile(num));
                 startActivity(i);
+
 
             }
         });
     }
 
-    void ListDir(File f) {
+    public void ListDir(File f) {
         File[] files = f.listFiles();
         fileList.clear();
         for (File file : files) {
@@ -56,6 +69,34 @@ public class FileAndDirectoryActivity extends ListActivity {
         }
         ArrayAdapter<String> directoryList = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, fileList);
         setListAdapter(directoryList);
+    }
+
+    public String ReadFile( String fileName){
+        String line = null;
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream (new File(path + "/"+fileName));
+            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while ( (line = bufferedReader.readLine()) != null )
+            {
+                stringBuilder.append(line + System.getProperty("line.separator"));
+            }
+            fileInputStream.close();
+            line = stringBuilder.toString();
+
+            bufferedReader.close();
+        }
+        catch(FileNotFoundException ex) {
+            Log.d(TAG, ex.getMessage());
+        }
+        catch(IOException ex) {
+            Log.d(TAG, ex.getMessage());
+        }
+        System.out.println(line);
+        return line;
     }
 
 
